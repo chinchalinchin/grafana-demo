bb553edd2a3e9bded89738c5b47b786936168b24
Grant Moore <grant.moore@ibm.com>
2019-09-25 11:50:58 -0400

    get rid of test panels that dont work

--- app.js
+++ app.js
@@ -6,14 +6,14 @@ const cors = require('cors')
 
 // GRAFANA INFO
 const grafanaHost = 'http://localhost:8088'
+const apiKey =  "eyJrIjoiZkU1ekF3ckRtVHhKN0xNbWRGQnJJa3FuS005cUd2U3YiLCJuIjoidGVzdF9rZXkiLCJpZCI6MX0="
 const grafanaOptions = {
-    apiKey = "eyJrIjoiZkU1ekF3ckRtVHhKN0xNbWRGQnJJa3FuS005cUd2U3YiLCJuIjoidGVzdF9rZXkiLCJpZCI6MX0=",
     getSnapshotUrl:`${grafanaHost}/api/snapshots`,
     postSnapshotUrl:`${grafanaHost}/api/snapshots/`,
     getAllSnapshotsUrl: `${grafanaHost}/api/dashboard/snapshots`,
     headers:{
       'Accept': 'application/json',
-      'Authorization': `Bearer ${key}`
+      'Authorization': `Bearer ${apiKey}`
     }
   }
 
@@ -24,17 +24,11 @@ app.use(bodyParser.json())
 app.use(cors())
 
 // ROUTING
-app.get('/redirect/getPanel/', function(req, res, next){ });
-
-app.get('/example/static-panel', function(req,res,next){
-    log("Serving grafana_static_panel.html", paths[3])
+app.get('/example/panel', function(req,res,next){
+    log("Serving grafana_static_panel.html", '/example/static-panel/')
     res.sendFile(path.join(__dirname, 'grafana_component', 'grafana_static_panel.html'))
 });
 
-app.get('/example/ajax-panel', function(req,res,next){
-    log("Serving grafana_ajax_panel.html", paths[5])
-    res.sendFile(path.join(__dirname, 'grafana_component', 'grafana_ajax_panel.html'))
-});
 
 // SERVER 
 app.listen(8000, function(){
