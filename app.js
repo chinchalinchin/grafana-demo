2076de454b125eeee734ea4875d61d69b8454781
Grant Moore <grant.moore@ibm.com>
2019-09-26 09:01:28 -0400

    testing out various ways of making anoynmous calls to grafana server. moved old techniques to /old/ folder instead of deleting incase i need to reference them

--- app.js
+++ app.js
@@ -20,34 +20,42 @@ app.get('/scripts/helper/', function(req, res, next){
   res.sendFile(path.join(__dirname, 'helper.js'))
 })
 
-  // GRAFANA REDIRECT
+  // GRAFANA AUTHENTICATED REDIRECT
 app.get('/grafana/getPanel', function(req, res, next){
-  helper.log("Requesting Panel From Grafana...", "/grafana/getPanel/")
+  helper.log("Requesting Authenticated Panel From Grafana...", "/grafana/getPanel/")
   const now = new Date().getTime()
   options = {
     url: helper.constructUrl(1, now, now - helper.calculateTimeDelta(), 2, 500, 500),
     header: helper.grafanaHeaders()
   }
-  request(options, function(grafReq, grafRes, next){
+
+  request(options, function(grafReq, grafRes, grafNext){
     helper.log("Response Received From Grafana...", "/grafana/getPanel/")
-    //console.log(grafRes.body)
+    res.send(grafRes)
+  })
+})
+
+  // GRAFANA ANONYMOUS REDIRECT
+app.get('/grafana/getAnonPanel', function(req, res, next){
+  helper.log("Requesting Anonynmous Panel From Grafana...", "/grafana/getAnonPanel/")
+  const now = new Date().getTime()
+  options = {
+    url: helper.constructUrl(1, now, now - helper.calculateTimeDelta(), 2, 500, 500),
+    headers:{
+      //Accept: 'img/png'
+    }
+  }
+  request(options, function(grafReq, grafRes, grafNext){
+    helper.log("Response Received From Grafana...", "/grafana/getAnonPanel/")
     res.send(grafRes)
   })
 })
 
   // HTML PAGES
-app.get('/example/static-panel', function(req, res, next){
+app.get('/example/panel', function(req, res, next){
     helper.log("Serving grafana_static_panel.html", '/example/static-panel/')
-    res.sendFile(path.join(__dirname, 'grafana_component', 'grafana_static_panel.html'))
+    res.sendFile(path.join(__dirname, 'grafana_component', 'grafana_panel.html'))
 });
-app.get('/example/ajax-panel', function(req, res, next){
-  helper.log('Serving grafana_ajax_panel.html', '/example/ajax-panel')
-  res.sendFile(path.join(__dirname, 'grafana_component', 'grafana_ajax_panel.html'))
-})
-app.get('/example/redirect-panel',function(req, res, next){
-  helper.log("Serving grafana_redirect_panel.html", 'example/redirect-panel')
-  res.sendFile(path.join(__dirname, 'grafana_component', 'grafana_redirect_panel.html'))
-})
 
 // SERVER 
   // LISTEN
